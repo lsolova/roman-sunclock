5b476d69f3d1c90e0694e3ebb4909048f28b360c
Laszlo Solova <laszlo@solova.com>
2024-10-29 17:11:36 +0100

    Self descriptive behavior and better failure tolerance.
    Signed-off-by: Laszlo Solova <laszlo@solova.com>

--- www/index.js
+++ www/index.js
@@ -31,18 +31,24 @@ function getLocation() {
   });
 }
 
-getLocation().then((locationDetails) => {
+function updateCurrentRomanSunTime(locationDetails) {
   const romanSunTime = wasm.roman_sun_time(
     BigInt(Date.now()),
     locationDetails.lat,
     locationDetails.lon,
     locationDetails.alt
   );
-  const dayOrNightElement = document.getElementById("dayOrNight");
+
+  const clockDetails = document.getElementById("clockDetails");
+
+  const dayOrNightElement = document.createElement("div");
+  dayOrNightElement.classList.add("RomanClock__DayOrNight");
   dayOrNightElement.classList.add(
     `RomanClock__DayOrNight--${romanSunTime.is_day ? "day" : "night"}`
   );
-  const nowTimeElement = document.getElementById("nowTime");
+
+  const nowTimeElement = document.createElement("div");
+  nowTimeElement.classList.add("RomanClock__Time");
   nowTimeElement.appendChild(
     document.createTextNode(
       `${Math.floor(romanSunTime.total_minutes / 60)
@@ -52,7 +58,9 @@ getLocation().then((locationDetails) => {
         .padStart(2, "0")}`
     )
   );
-  const minuteLengthElement = document.getElementById("minuteLength");
+
+  const minuteLengthElement = document.createElement("div");
+  minuteLengthElement.classList.add("RomanClock__MinuteLength");
   minuteLengthElement.appendChild(
     document.createTextNode(
       `${new Intl.NumberFormat("en", { maximumFractionDigits: 2 }).format(
@@ -68,4 +76,27 @@ getLocation().then((locationDetails) => {
   console.log(lastSunChange, new Date(Number(lastSunChange)));
   console.log(nextSunChange, new Date(Number(nextSunChange)));
   console.log(romanSunTime.hours, romanSunTime.minutes);
+
+  let firstChild = null;
+  while ((firstChild = clockDetails.firstChild) !== null) {
+    clockDetails.removeChild(firstChild);
+  }
+  clockDetails.appendChild(dayOrNightElement);
+  clockDetails.appendChild(nowTimeElement);
+  clockDetails.appendChild(minuteLengthElement);
+
+  return romanSunTime.minute_length;
+}
+
+getLocation().then((locationDetails) => {
+  const intervalLength = updateCurrentRomanSunTime(locationDetails);
+  setInterval(
+    () => updateCurrentRomanSunTime(locationDetails),
+    intervalLength * 1000 || 1000
+  );
+}).catch((e) => {
+  const notification = document.querySelector("#clockDetails .RomanClock__Notification");
+  notification.innerHTML = "Cannot get your location. This clock cannot work without this information. Please allow it and refresh the page.";
+
+  console.log(e);
 });
