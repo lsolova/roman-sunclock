be105b887328772663e57ef097c16336d9306b99
Laszlo Solova <laszlo@solova.com>
2024-12-03 21:42:02 +0100

    Generate sun, moon icons and hour texts within SVG - Additional fix of Chrome preview background text visibility

--- src/lib.rs
+++ src/lib.rs
@@ -18,18 +18,7 @@ pub fn roman_sun_time(
 ) -> types::RomanSunclockResult {
     let time_details = sun_time_calculator::calculate_roman_sun_time(current_epoch, lat, lon, alt);
 
-    let sunrise_epoch = if time_details.is_day {
-        time_details.last_sun_change
-    } else {
-        time_details.next_sun_change
-    };
-    let sunset_epoch = if time_details.is_day {
-        time_details.next_sun_change
-    } else {
-        time_details.last_sun_change
-    };
-
-    let clock_svg = generate_svg_clock(current_epoch, sunrise_epoch, sunset_epoch, timezone_offset);
+    let clock_svg = generate_svg_clock(current_epoch, timezone_offset, &time_details);
     RomanSunclockResult {
         time_details,
         clock_svg,
