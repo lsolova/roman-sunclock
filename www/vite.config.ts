4eb828b8ad122e899d23da57fe150d3b5ba9267d
Laszlo Solova <laszlo@solova.com>
2024-12-09 20:32:42 +0100

    Build installable PWA

--- www/vite.config.ts
+++ www/vite.config.ts
@@ -26,13 +26,36 @@ export default defineConfig({
       devOptions: {
         enabled: true,
       },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
       manifest: {
         name: "Roman Sunclock Time",
         short_name: "Sunclock",
         description:
           "Displays time following the roman sunclock time calculations (12 hours daytime, 12 hours nighttime).",
         id: "com.solova.rsct",
-        theme_color: "#FFFFFF",
+        icons: [
+          {
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
       },
       registerType: "autoUpdate",
       workbox: {
